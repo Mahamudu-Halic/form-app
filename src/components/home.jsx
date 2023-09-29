ac033a5ef73841aff26565602d38887848b89df4
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-09-29 10:17:01 +0000

    new param

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
                         <Route path="/courses/:param/:year/:route" element={<DownloadFile />} />
                     </Routes>
 
-                    {/* <Footer /> */}
+                    <Footer />
                     
                 </div>
             </div>
